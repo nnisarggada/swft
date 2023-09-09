82ccd8e4daa29929eef1bd8960bce7925a897deb
nnisarggada <gadannisarg@gmail.com>
2023-09-09 23:38:23 +0530

    added file duration via cli, max file size, allowed types, webshare api

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template, send_from_directory, make_response
 from werkzeug.utils import secure_filename
 import os
 import time
@@ -9,12 +9,33 @@ import json
 # The following variables need to be changed before running the app:
 # -------------------------------------------------------------------
 
-port = 80 # Port that the app will run on
-url = "localhost" # Url of the hosted app
-folder = "/home/nnisarggada/swft/share_temp" # Folder where the files will stored temporarily
-del_time = 1800 # Time until files will be deleted in seconds
-
+PORT = 80 # Port that the app will run on
+URL = "localhost" # Url of the hosted app
+TEMP_FOLDER = "/home/nnisarggada/GitRepos/swft/share_temp" # Folder where the files will stored temporarily
+DEFAULT_DEL_TIME = 1800 # Time until files will be deleted in seconds
+ALLOWED_EXTENSIONS = {
+    'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif',  # Images and Documents
+    'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx',  # Office Documents
+    'zip', 'rar', 'tar', 'gz',  # Compressed Files
+    'mp3', 'wav', 'ogg', 'flac',  # Audio Files
+    'mp4', 'mov', 'avi', 'mkv',  # Video Files
+    'csv', 'tsv',  # Data Files
+    'html', 'htm', 'xml', 'json',  # Web Formats
+    'css', 'js', 'py', 'java', 'cpp',  # Code Files
+    'txt', 'log', 'ini', 'cfg',  # Config and Text Files
+    'md', 'markdown', 'rst',  # Markup Languages
+    'sql', 'db', 'sqlite', 'dbf',  # Database Files
+}
+MAX_CONTENT_LENGTH = 64 * 1024 * 1024  # 64MB
+MAX_DEL_TIME = 24 * 60 * 60  # 24 hours in seconds
 # -------------------------------------------------------------------
+
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = TEMP_FOLDER
+
+# Dictionary to track files and their individual deletion times
+files_managed = {}
 
 def save_files_managed_to_file():
     with open('files_managed.json', 'w') as json_file:
@@ -26,27 +47,35 @@ def load_files_managed_from_file():
             return json.load(json_file)
     return {}
 
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = folder
-files_managed = load_files_managed_from_file()
+def is_allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def generate_unique_filename(filename):
+    base, ext = os.path.splitext(filename)
+    counter = 1
+    while os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], filename)):
+        filename = f"{base}_{counter}{ext}"
+        counter += 1
+    return filename
 
 def delete_old_files():
     while True:
         current_time = time.time()
         to_delete = []
-        
-        for link, filename in files_managed.items():
+
+        for link, (filename, del_time) in files_managed.items():
             file_creation_time = os.path.getctime(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             if current_time - file_creation_time >= del_time:
                 to_delete.append(link)
-        
+
         for link in to_delete:
-            filename = files_managed.pop(link)
+            filename, _ = files_managed.pop(link)
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             os.remove(file_path)
 
+        # Save the updated files_managed dictionary to a file
         save_files_managed_to_file()
-        
+
         time.sleep(10)
 
 file_management_thread = threading.Thread(target=delete_old_files)
@@ -55,7 +84,7 @@ file_management_thread.start()
 
 @app.route('/', methods=['GET'])
 def upload_page():
-    return render_template('index.html', full_url=url)
+    return render_template('index.html', full_url=URL)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -67,35 +96,42 @@ def upload_file():
     if uploaded_file.filename == '':
         return 'No selected file\n', 400
 
-    ext = uploaded_file.filename.rsplit('.', 1)[1].lower()
-    
-    current_time = int(time.time())
+    if not is_allowed_file(uploaded_file.filename):
+        return 'File type not allowed\n', 400
 
-    custom_link = request.form.get('link', None)
+    del_time = int(request.form.get('time', DEFAULT_DEL_TIME))
+    del_time = min(del_time, MAX_DEL_TIME)
+
+    filename = secure_filename(generate_unique_filename(uploaded_file.filename))
+
+    custom_link = request.form.get('link', filename)
+
 
     if custom_link == "":
-        custom_link = f"{current_time}"
+        custom_link = f"{filename}"
 
-    if custom_link in files_managed:
+    if custom_link in files_managed or custom_link == "upload" or custom_link == "qr":
         return f"Link {custom_link} already exists\n", 400
 
-    filename = secure_filename(f"{current_time}.{ext}")
 
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
 
     try:
         uploaded_file.save(file_path)
 
-        link = f'{url}/{custom_link}\n'
+        link = f'{URL}/{custom_link}\n'
         clickable_link = f'\033]8;;{link}\033\\{link}\033]8;;\033\\'
 
-        files_managed[custom_link] = filename
+        # Store the file information along with its deletion time
+        files_managed[str(custom_link)] = (filename, del_time)
+        print(files_managed)
 
         user_agent = request.headers.get('User-Agent')
         if 'curl' in user_agent.lower() or 'wget' in user_agent.lower():
             return clickable_link
         else:
-            return render_template('shared.html', url=url, link=custom_link)
+            return render_template('shared.html', url=URL, link=custom_link)
 
     except Exception as e:
         return f"Error: {e}\n", 500
@@ -103,8 +139,9 @@ def upload_file():
 @app.route('/<link>', methods=['GET'])
 def share_file(link):
     if link not in files_managed:
+        print(link)
         return "Invalid link\n", 400
-    return send_from_directory(app.config['UPLOAD_FOLDER'], files_managed[link], as_attachment=True)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], files_managed[link][0], as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=PORT)
